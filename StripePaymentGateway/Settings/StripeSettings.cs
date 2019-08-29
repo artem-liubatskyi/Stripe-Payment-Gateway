f144b9a9b7c9a3045879ab21b24eecafc9c7287e
Artem Liubatskyi <deadly241@gmail.com>
2019-08-29 19:32:51 +0300

    Added Stripe

---
+++ StripePaymentGateway/Settings/StripeSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace StripePaymentGateway.Settings
+{
+    public class StripeSettings
+    {
+        public string SecretKey { get; set; }
+        public string PublishableKey { get; set; }
+    }
+}
--- StripePaymentGateway/Startup.cs
+++ StripePaymentGateway/Startup.cs
@@ -1,14 +1,25 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Stripe;
+using StripePaymentGateway.Settings;
 
 namespace StripePaymentGateway
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
         public void ConfigureServices(IServiceCollection services)
         {
+            var stripeSection = Configuration.GetSection("StripeSettings");
+            var stripeSettings = stripeSection.Get<StripeSettings>();
+            StripeConfiguration.ApiKey = stripeSettings.SecretKey;
         }
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
